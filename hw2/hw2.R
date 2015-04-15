fe720c56a0b59192513be1feae6c0ca6f1976079
Will Clark <wclark3@gmail.com>
2015-04-14 21:05:55 -0500

    added regressions w/ fdr cutoff for question 2

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -1,97 +1,122 @@
 ## Big Data HW 2
 
 source('../utils/source_me.R', chdir = T)
+source("../utils/fdr.R")
 OutputToFile = T
 
-## read data
+## READ DATA
 homes <- read.csv('homes2004.csv')
 
-## organize data
+## ORGANIZE DATA
 
 # dependent variable: log of price 
 y <- log(homes$VALUE)
 
-# independent variables
-x <- homes
-x$LAMMORT <- log(x$AMMORT) # log of amt of first mortgage
-x$LPRICE <- log(x$LPRICE) # log of land + unit price
-x$LZINC2 <- log(x$ZINC2) # log of household income
-x$AMMORT <- NULL # drop variables that are tracked in logs (above)
-x$ZINC2 <- NULL
-x$VALUE <- NULL # drop dependent variable
-
-## make some interesting plots
+## Q1: DATA VISUALIZATION
+#  Plot some relationships and tell a story
 
 #par(mfrow=c(1,2))
 
 # plot home value vs education level
-x$HHGRAD <- factor(x$HHGRAD, levels=c("No HS", "HS Grad", "Assoc", "Bach", "Grad"))
+homes$HHGRAD <- factor(homes$HHGRAD, levels=c("No HS", "HS Grad", "Assoc", "Bach", "Grad"))
 PlotSetup('hhgrad')
-plot(y~x$HHGRAD, col=rainbow(nlevels(x$HHGRAD)), outline=F, 
+plot(y~homes$HHGRAD, col=rainbow(nlevels(homes$HHGRAD)), outline=F, 
      main="", xlab="Education Level of Homeowner", ylab="Current market value (log)")
 PlotDone()
 
 # plot home value vs household income and show college degree status
 # for plotting only, make a dummy var for has college degree
-x$COLLEGE <- x$HHGRAD
-levels(x$COLLEGE) <- list("No College"=c("No HS", "HS Grad", "Assoc"), 
+homes$COLLEGE <- homes$HHGRAD
+levels(homes$COLLEGE) <- list("No College"=c("No HS", "HS Grad", "Assoc"), 
                           "College"=c("Bach", "Grad"))
 
 PlotSetup('income')
-plot(y~x$LZINC2, col=c("blue","red")[as.numeric(x$COLLEGE)],
+plot(y~log(homes$ZINC2), col=c("blue","red")[as.numeric(homes$COLLEGE)],
      main="", xlab="Income of Homeowner (log)", ylab="Current market value (log)")
-legend("bottomleft", legend=levels(x$COLLEGE), pch=c(1,1),
+legend("bottomleft", legend=levels(homes$COLLEGE), pch=c(1,1),
        col=c("blue","red"))
 PlotDone()
 
+homes$COLLEGE <- NULL
+
 # first home and downpayment source (same story)
 PlotSetup('frstho')
-plot(y~x$FRSTHO, outline=F, varwidth=T, col=c("red","blue"),
+plot(y~homes$FRSTHO, outline=F, varwidth=T, col=c("red","blue"),
      main="", xlab="Buyer's First Home?", ylab="Current market value (log)")
 PlotDone()
 
 # change order of level to match first home y/n
-x$DWNPAY <- factor(x$DWNPAY, c("prev home","other"))
+homes$DWNPAY <- factor(homes$DWNPAY, c("prev home","other"))
 PlotSetup('dwnpay')
-plot(y~x$DWNPAY, outline=F, varwidth=T, col=c("red","blue"),
+plot(y~homes$DWNPAY, outline=F, varwidth=T, col=c("red","blue"),
      main="", xlab="Downpayment Source", ylab="Current market value (log)")
 PlotDone()
 
 # of bathrooms/bedrooms (same story as well)
 PlotSetup('baths')
-boxplot(y~x$BATHS, outline=F, varwidth=T, col=rainbow(nlevels(as.factor(x$BATHS))),
+boxplot(y~homes$BATHS, outline=F, varwidth=T, col=rainbow(nlevels(as.factor(homes$BATHS))),
         main="", xlab="# of Bathrooms", ylab="Current market value (log)")
 PlotDone()
 
 PlotSetup('bedrms')
-boxplot(y~x$BEDRMS, outline=F, varwidth=T, col=rainbow(nlevels(as.factor(x$BEDRMS))),
+boxplot(y~homes$BEDRMS, outline=F, varwidth=T, col=rainbow(nlevels(as.factor(homes$BEDRMS))),
         main="", xlab="# of Bedrooms", ylab="Current market value (log)")
 PlotDone()
 
 # neighborhood quality factors
 # howH, howN, eaban, ejunk, ecom2
 PlotSetup('howh')
-boxplot(y~x$HOWH, outline=F, varwidth=T, col=c("red", "blue"),
+boxplot(y~homes$HOWH, outline=F, varwidth=T, col=c("red", "blue"),
         main="", xlab="Unit Quality", ylab="Current market value (log)")
 PlotDone()
 
 PlotSetup('hown')
-boxplot(y~x$HOWN, outline=F, varwidth=T, col=c("red", "blue"),
+boxplot(y~homes$HOWN, outline=F, varwidth=T, col=c("red", "blue"),
         main="", xlab="Neighborhood Quality", ylab="Current market value (log)")
 PlotDone()
 
 # plot some neighborhood quality metrics
 # cheap way to see which difference is largest between these three
-#a <- mean(y[x$EABAN=="Y"])-mean(y[x$EABAN=="N"])
-#b <- mean(y[x$EJUNK=="Y"])-mean(y[x$EJUNK=="N"])
-#c <- mean(y[x$ECOM2=="Y"])-mean(y[x$ECOM2=="N"])
+#a <- mean(y[homes$EABAN=="Y"])-mean(y[homes$EABAN=="N"])
+#b <- mean(y[homes$EJUNK=="Y"])-mean(y[homes$EJUNK=="N"])
+#c <- mean(y[homes$ECOM2=="Y"])-mean(y[homes$ECOM2=="N"])
 
 PlotSetup('eaban')
-plot(y~x$EABAN, outline=F, varwidth=T, col=c("red", "blue"),
+plot(y~homes$EABAN, outline=F, varwidth=T, col=c("red", "blue"),
      main="", xlab="Close to Abandoned Building?", ylab="Current market value (log)")
 PlotDone()
 
 PlotSetup('ejunk')
-plot(y~x$EJUNK, outline=F, varwidth=T, col=c("red", "blue"),
+plot(y~homes$EJUNK, outline=F, varwidth=T, col=c("red", "blue"),
      main="", xlab="Trash/Junk in Street?", ylab="Current market value (log)")
 PlotDone()
+
+## Q2: SIMPLE REGRESSION
+#  Regress log value onto all but mortgage and purchase $
+#  How many coefficients are significant at 10% FDR?
+#  Re-run regression with only the significant covariates
+
+reg1 <- glm(log(VALUE) ~ .-AMMORT-LPRICE, data=homes)
+
+# -1 to drop the intercept
+coefs <- summary(reg1)$coef
+coefs <- coefs[-1,]
+
+## grab the non-intercept p-values from a glm
+# Extract just the p-values from the 4th column
+pvals <- coefs[,4]
+
+# Calculate the False Discovery Rate Alpha Parameter
+q = 10/100
+alpha <- fdr_cut(pvals, q = q, plotit = TRUE)
+
+# Get a list of just the significant variables.
+signif <- coef(reg1)[-1][pvals<=alpha]
+
+# How many true discoveries?
+print(paste("True discoveries:", length(signif)))
+
+## Re-fit model with only significant covariates
+mm <- model.matrix( ~.-AMMORT-LPRICE, data=homes)[,-1]
+reg2 <- glm( log(mm[,"VALUE"]) ~ ., data=as.data.frame(mm)[, names(signif)])
+print(summary(reg2))
