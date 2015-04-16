7d8b2955b959111ef33b697d8e15ef8850b17292
Will Clark <wclark3@gmail.com>
2015-04-16 13:50:40 -0500

    add function glm_fdr to automaet fdr recalcs

--- utils/fdr.R
+++ utils/fdr.R
@@ -19,3 +19,29 @@ fdr_cut <- function(pvals, q, plotit=FALSE, filename='fdr_cut') {
   
   return(alpha)
 }
+
+## re-compute glm for covariates above p-value cutoff for a given FDR (q)
+glm_fdr <- function(reg_in, q, plotit=FALSE) {
+  
+  coefs <- summary(reg_in)$coef # -1 to drop the intercept
+  coefs <- coefs[-1,]
+  
+  ## grab the non-intercept p-values from a glm
+  # Extract just the p-values from the 4th column
+  pvals <- coefs[,4]
+  
+  # Calculate the False Discovery Rate Alpha Parameter
+  alpha <- fdr_cut(pvals, q = q, plotit)
+  print(alpha)
+  
+  # Get a list of just the significant variables.
+  signif <- coef(reg1)[-1][pvals<=alpha]
+  
+  ## Re-fit model with only significant covariates
+  mm <- model.matrix(~., data=reg_in$model) #[,-1] # -1 to drop the intercept
+  reg_out <- glm( reg_in$y ~ ., data=as.data.frame(mm)[, names(signif)])
+  
+  print(summary(reg_out))
+  
+  #return(reg_out)
+}
